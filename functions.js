a9c8047b47403b60acea56ddecfec24e9cf4ee55
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-04-01 20:52:13 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,16 @@
+//functions:
+var x=10;
+var y=20;
+
+function sum(){
+    console.log(x+y);  
+}
+sum();
+
+//functiion with parameters
+
+function adding_2nums(){
+  console.log(x+y);  
+}
+adding_2nums(12,121)
+adding_2nums(56,9)
